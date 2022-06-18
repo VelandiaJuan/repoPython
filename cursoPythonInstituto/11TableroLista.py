42adef6a9e0a7be9280c3d26f3c4f7e67116c867
VelandiaJuan <pablo.velandia@bcpgrp.com>
2022-06-17 23:17:40 -0500

    Adicion de Ejercios de Listas

---
+++ cursoPythonInstituto/11TableroLista.py
@@ -0,0 +1,18 @@
+EMPTY = "-"
+PAWN = "PEON"
+ROOK = "TORRE"
+KNIGHT = "CABALLO"
+board = []
+
+for i in range(8):
+    row = [EMPTY for i in range(8)]
+    board.append(row)
+
+board[0][0] = ROOK
+board[0][7] = ROOK
+board[7][0] = ROOK
+board[7][7] = ROOK
+
+print(board)
+rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
+
